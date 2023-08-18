555005ba53feba22581e1fa4aa86579c4d3b3ae1
Taranga Manjunath Naik <taranganaik32@gmail.com>
2023-08-18 22:25:10 +0530

    StudentIDSort

---
+++ Galaxy/Program.cs
@@ -0,0 +1,10 @@
+﻿using Galaxy;
+
+StudentRandomIdSort objStudentRandomIdSort = new StudentRandomIdSort();
+List<int> stId=objStudentRandomIdSort.GetRandomNumber();
+
+stId.Sort();
+for (int i = 0; i < stId.Count; i++)
+{
+    Console.WriteLine($"Student ID : {stId[i]}");
+}
---
+++ Galaxy/StudentRandomIdSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxy
+{
+    internal class StudentRandomIdSort
+    {
+        Random objRandom = new Random();
+
+        public List<int> GetRandomNumber()
+        {
+            List<int> result = new List<int>();
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    result.Add(objRandom.Next(10));
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return result;
+        }
+    }
+}
